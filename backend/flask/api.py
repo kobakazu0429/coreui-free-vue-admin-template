d44cf52072bc93f9eec70dc0d2b91ffb08f8951f
kobakakzu0429 <kobakazu10@gmail.com>
2018-07-24 22:19:12 +0900

    create foregin api:GET

---
+++ backend/flask/api.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+import sys
+sys.path.append("..")
+
+from flask import Flask, jsonify, make_response
+from flask_cors import CORS
+
+from backend.config import settings
+from backend.flask.foreign import *
+
+api = Flask(__name__)
+CORS(api)
+
+
+# 404の時は以下を返す
+@api.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+# 処理する前にdbへ接続する
+@api.before_request
+def before_request_handler():
+    db.connect()
+
+
+# 処理した後にdbを切断する
+@api.teardown_request
+def after_request_handler(exc):
+    if not db.is_closed():
+        db.close()
+
+
+# return foreigns
+@api.route('/api/<string:foreign>', methods=['GET'])
+def swictherReturn(foreign):
+    if (foreign == 'types'):
+        return make_response(retuenType())
+    elif (foreign == 'groups'):
+        return make_response(retuenGroup())
+    elif (foreign == 'categories'):
+        return make_response(retuenCategory())
+    elif (foreign == 'formats'):
+        return make_response(retuenFormat())
+    elif (foreign == 'attributes'):
+        return make_response(retuenAttribute())
+    else:
+        return make_response(jsonify({'error': 'not exist this table [%s]' % foreign}), 404)
+
+
+if __name__ == '__main__':
+    api.run(host='0.0.0.0', port=settings.FLASK_PORT)
---
+++ backend/flask/foreign.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+from flask import jsonify, make_response
+
+from backend.models.Type import *
+from backend.models.Group import *
+from backend.models.Category import *
+from backend.models.Format import *
+from backend.models.Attribute import *
+
+
+def retuenType():
+    data = []
+    for x in Type.select().dicts().order_by(Type.id.asc()):
+        data.append(x)
+    return jsonify(data)
+
+
+def retuenGroup():
+    data = []
+    for x in Group.select().dicts().order_by(Group.id.asc()):
+        data.append(x)
+    return jsonify(data)
+
+
+def retuenCategory():
+    data = []
+    for x in Category.select().dicts().order_by(Category.id.asc()):
+        data.append(x)
+    return jsonify(data)
+
+
+def retuenFormat():
+    data = []
+    for x in Format.select().dicts().order_by(Format.id.asc()):
+        data.append(x)
+    return jsonify(data)
+
+
+def retuenAttribute():
+    data = []
+    for x in Attribute.select().dicts().order_by(Attribute.id.asc()):
+        data.append(x)
+    return jsonify(data)
