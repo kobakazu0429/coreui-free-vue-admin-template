9a4f9e9d2ad0b8c304880f43e089280d8faff51f
kobakakzu0429 <kobakazu10@gmail.com>
2018-08-01 22:33:47 +0900

    auto change end point url by env(dev)

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -13,8 +13,17 @@ import axios from 'axios'
 import VueAxios from 'vue-axios'
 import VueMoment from 'vue-moment'
 
-// todo
-// cssVars()
+switch (process.env.NODE_ENV) {
+  case 'development':
+    axios.defaults.baseURL = 'http://0.0.0.0:5000'
+    break
+  case 'production':
+    axios.defaults.baseURL = ''
+    break
+  default:
+    axios.defaults.baseURL = ''
+    break
+}
 
 Vue.use(BootstrapVue)
 Vue.use(VueAxios, axios)
