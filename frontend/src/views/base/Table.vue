9594e50cbbb88c280426e05311b38e85e65366d0
kobakakzu0429 <kobakazu10@gmail.com>
2018-08-01 23:41:24 +0900

    削除と編集をできるようにした

--- frontend/src/views/base/Table.vue
+++ frontend/src/views/base/Table.vue
@@ -1,55 +1,63 @@
 <template>
-    <b-card :header="caption">
-      <b-form-group class="mb-3">
-        <b-input-group>
-          <b-form-input v-model="filter" placeholder="Type to Search" />
-          <b-input-group-append>
-            <b-btn :disabled="!filter" @click="filter = ''">Clear</b-btn>
-          </b-input-group-append>
-        </b-input-group>
-      </b-form-group>
-      <b-table
-        :hover="hover"
-        :striped="striped"
-        :bordered="bordered"
-        :small="small"
-        :fixed="fixed"
-        :fields="fields"
-        :items="items"
-        :sort-by.sync="sortBy"
-        :sort-desc.sync="sortDesc"
-        :filter="filter"
-        @filtered="onFiltered"
-        responsive="sm"
-      >
-        <template slot="attribute" slot-scope="data">
-          <a :href="data.item.attribute_utl">{{data.item.attribute_title}}</a>
-        </template>
-        <template slot="status" slot-scope="data">
-        </template>
-        <template slot="more-info" slot-scope="data">
-          <b-btn @click="showModal('modal'+data.item.id)">詳細を開く</b-btn>
-          <b-modal :ref="'modal'+data.item.id" title="詳細"  hide-footer>
+  <b-card :header="caption">
+    <b-form-group class="mb-3">
+      <b-input-group>
+        <b-form-input v-model="filter" placeholder="Type to Search" />
+        <b-input-group-append>
+          <b-btn :disabled="!filter" @click="filter = ''">Clear</b-btn>
+        </b-input-group-append>
+      </b-input-group>
+    </b-form-group>
+    <b-table :hover="hover" :striped="striped" :bordered="bordered" :small="small" :fixed="fixed" :fields="fields" :items="items" :sort-by.sync="sortBy" :sort-desc.sync="sortDesc" :filter="filter" @filtered="onFiltered" responsive="sm">
+      <template slot="attribute" slot-scope="data">
+        <a :href="data.item.attribute_utl">{{data.item.attribute_title}}</a>
+      </template>
+      <template slot="status" slot-scope="data">
+      </template>
+      <template slot="more-info" slot-scope="data">
+        <b-btn @click="showModal('modal'+data.item.id)">詳細を開く</b-btn>
+        <b-modal :ref="'modal'+data.item.id" title="詳細" hide-footer>
+          <p>id : {{data.item.id}}</p>
+          <p>name : {{data.item.name}}</p>
+          <p>description : {{data.item.description}}</p>
+          <p>url : {{data.item.url}}</p>
+          <p>type : {{data.item.type}}</p>
+          <p>group : {{data.item.group}}</p>
+          <p>category : {{data.item.category}}</p>
+          <p>format : {{data.item.format}}</p>
+          <p>attribute_title : {{data.item.attribute_title}}</p>
+          <p>attribute_utl : {{data.item.attribute_utl}}</p>
+        </b-modal>
+      </template>
+      <template slot="updated_at_moment" slot-scope="data">
+        <p>{{$moment(data.item.updated_at).tz('Etc/GMT').format('YYYY/MM/DD - HH:mm:ss')}}</p>
+      </template>
+      <template slot="created_at_moment" slot-scope="data">
+        <p>{{$moment(data.item.created_at).tz('Etc/GMT').format('YYYY/MM/DD - HH:mm:ss')}}</p>
+      </template>
+      <template slot="actions" slot-scope="data">
+        <b-button class="mr-3" variant="outline-primary" @click="showModal('edit-modal'+data.item.id)">編集</b-button>
+        <b-button class="mr-3" variant="outline-danger" @click="$emit('delete-id', data.item.id)">削除</b-button>
+        <b-modal :ref="'edit-modal'+data.item.id" title="編集" hide-footer>
+          <div class="modal-body">
             <p>id : {{data.item.id}}</p>
-            <p>name : {{data.item.name}}</p>
-            <p>description : {{data.item.description}}</p>
-            <p>url : {{data.item.url}}</p>
-            <p>type : {{data.item.type}}</p>
-            <p>group : {{data.item.group}}</p>
-            <p>category : {{data.item.category}}</p>
-            <p>format : {{data.item.format}}</p>
-            <p>attribute_title : {{data.item.attribute_title}}</p>
-            <p>attribute_utl : {{data.item.attribute_utl}}</p>
-          </b-modal>
-        </template>
-        <template slot="updated_at_moment" slot-scope="data">
-          <p>{{$moment(data.item.updated_at).format('YYYY/MM/DD - HH:mm:ss')}}</p>
-        </template>
-        <template slot="created_at_moment" slot-scope="data">
-          <p>{{$moment(data.item.created_at).format('YYYY/MM/DD - HH:mm:ss')}}</p>
-        </template>
-      </b-table>
-    </b-card>
+            <b-form-group class="mb-3">
+              <b-input-group>
+                <b-form-input v-model="edited" :placeholder="'現在は「' + data.item.type + '」です'" />
+                <b-input-group-append>
+                  <b-btn :disabled="!edited" @click="edited = ''">Clear</b-btn>
+                </b-input-group-append>
+              </b-input-group>
+            </b-form-group>
+          </div>
+          <div class="modal-footer">
+            <b-button variant="secondary" data-dismiss="modal" @click="hideModal('edit-modal'+data.item.id)">キャンセル</b-button>
+            <b-button variant="success" @click="$emit('edit-id', {id:data.item.id,edited:edited})">保存</b-button>
+          </div>
+        </b-modal>
+      </template>
+    </b-table>
+  </b-card>
 </template>
 
 <script>
@@ -58,7 +66,8 @@ export default {
   data() {
     return {
       sortDesc: false,
-      filter: null
+      filter: null,
+      edited: null
     }
   },
   methods: {
@@ -71,6 +80,9 @@ export default {
     },
     showModal(id) {
       this.$refs[id].show()
+    },
+    hideModal(id) {
+      this.$refs[id].hide()
     }
   },
   props: {
--- frontend/src/views/settings/Types.vue
+++ frontend/src/views/settings/Types.vue
@@ -1,14 +1,22 @@
 <template>
   <div class="animated fadeIn">
-      <b-col lg="12">
-        <c-table
-          bordered
-          caption="Types"
-          :fields="fields"
-          :items="items"
-          :sort-by.sync="sortBy"
-        ></c-table>
-      </b-col>
+    <b-col lg="12">
+      <b-card header="新規追加">
+        <b-form inline onsubmit="return false;">
+          <label class="mr-sm-2" for="inlineInput1">タイプ:</label>
+          <b-input id="inlineInput1" class="mr-3" type="text" placeholder="overlay" v-model="input"></b-input>
+          <b-button type="submit" size="sm" variant="primary" class="mr-3" @click="post">
+            <i class="fa fa-dot-circle-o"></i>
+            Submit
+          </b-button>
+          <b-button type="reset" size="sm" variant="danger">
+            <i class="fa fa-ban"></i>
+            Reset
+          </b-button>
+        </b-form>
+      </b-card>
+      <c-table bordered caption="Types" :fields="fields" :items="items" :sort-by.sync="sortBy" @delete-id="delete_column" @edit-id="edit_column"></c-table>
+    </b-col>
   </div>
 </template>
 
@@ -18,8 +26,9 @@ import cTable from '../base/Table'
 const fields = [
   { key: 'id', label: 'ID', sortable: true },
   { key: 'type', label: 'タイプ', sortable: true },
-  { key: 'updated_at_moment', label: '作成日' },
-  { key: 'created_at_moment', label: '更新日' }
+  { key: 'updated_at_moment', label: '更新日' },
+  { key: 'created_at_moment', label: '作成日' },
+  { key: 'actions', label: '操作' }
 ]
 
 export default {
@@ -31,13 +40,50 @@ export default {
     return {
       sortBy: 'id',
       fields: fields,
-      items: null
+      items: null,
+      input: null
     }
   },
   mounted: function() {
-    this.axios.get('http://0.0.0.0:5000/api/types/').then(response => {
+    this.axios.get('/api/types/').then(response => {
       this.items = response.data
     })
+  },
+  methods: {
+    post: function() {
+      this.axios
+        .post('/api/types/', {
+          type: this.input
+        })
+        .then(response => {
+          location.reload()
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    },
+    delete_column: function(id) {
+      this.axios
+        .delete(`/api/types/${id}/`)
+        .then(response => {
+          location.reload()
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    },
+    edit_column: function(params) {
+      this.axios
+        .patch(`/api/types/${params.id}/`, {
+          type: params.edited
+        })
+        .then(response => {
+          location.reload()
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    }
   }
 }
 </script>
