9c6d54444d40fab98780f219567bea4285cbf832
kobakakzu0429 <kobakazu10@gmail.com>
2018-08-03 10:51:09 +0900

    prettierの設定の追加

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,17 +1,23 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
+    browser: true,
+    es6: true
   },
-  'extends': [
-    'plugin:vue/essential',
-    'eslint:recommended'
-  ],
+  extends: ['eslint:recommended', 'plugin:vue/recommended', 'plugin:vue/essential'],
+  plugins: ['vue'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'linebreak-style': ['error', 'unix'],
+    indent: ['error', 2],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never']
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: 'babel-eslint',
+    ecmaVersion: 6,
+    sourceType: 'module'
   }
 }
